c1bb81c48f0ef8fca2712dd7293a5f11909fe737
webvpro <mlstanley@gmail.com>
2025-06-01 10:28:00 -0700

    init vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+	adapter: vercel(),
 	integrations: [
 		starlight({
 			title: 'Docs with Tailwind',
